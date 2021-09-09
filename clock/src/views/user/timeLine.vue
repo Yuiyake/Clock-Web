fcc3b2d3579aa025943676402766830e4d9bfa28
yuiyake <1306824899@qq.com>
2021-09-09 17:15:36 +0800

    '评论组件数据连接问题'

--- clock/src/views/user/timeLine.vue
+++ clock/src/views/user/timeLine.vue
@@ -74,7 +74,7 @@
 import {selectAllDynamic, deleteDynamic, selectThisDynamic} from "@/api/dynamic";
 import {selectUserReply, addRootReply, addSonReply} from "@/api/reply";
 import dynamicInfo from "@/views/admin/dynamicInfo";
-import * as CommentData from '../../components/mockdata'
+// import * as CommentData from '../../components/mockdata'
 import comment from "@/components/comment";
 export default {
   name: "timeLine",
@@ -86,15 +86,11 @@ export default {
       admin:{},
       commentData: [],
       dynamicData: [
-        {
-          // uid: '',
-          did: '',
-          // dconcern: '',
-          // support: '',
-          // dtime: '',
-          // dreplycount: '',
-        }
-      ]
+        {}
+      ],
+      didList:[],
+      // test:[],
+      CommentData:{},
     }
   },
   components: {
@@ -103,8 +99,9 @@ export default {
   created() {
     this.getUserInfo();
     this.getDynamic();
-    this.commentData = CommentData.comment.data;
-    this.getAllReply();
+    // this.commentData = CommentData.comment.data;
+    // CommentData.comment.data = this.commentData;
+    // this.getAllReply();
   },
   methods: {
     getDynamic(uid) {
@@ -114,15 +111,25 @@ export default {
       selectThisDynamic(this.dynamicData.uid).then(res => {
         let code = res.data.code
         // console.log(res.data.data)
-        if (code == 200){
+        if (code == 200) {
           this.dynamicData = res.data.data
           // console.log(this.dynamicData)
+          for (let i = 0; i < this.dynamicData.length; i++) {
+            this.didList.push(this.dynamicData[i].did)
+          }
+          // console.log("this.did:"+this.didList)
           // this.$message({showClose:true, message:'成功', type:'success'})
         } else {
-          this.$message({ showClose: true, message: '查询失败，请重试!', type: 'error'});
+          this.$message({showClose: true, message: '查询失败，请重试!', type: 'error'});
         }
-      }).catch((err) => {
-        console.log(err)
+        // 原因是getDynamic里发起网络请求是个异步的操作,你在created里按顺序调用了this.getDynamic和this.getAllReply
+        // 你用了同步的思维看待问题了,你以为在调用this.getAllReply的时候,已经执行了上面的selectThisDynamic(this.dynamicData.uid).then()内的代码
+        // but...网络请求是异步的
+        // 明白了就好
+        // Promise这样又then又catch的也比较繁琐
+        // 实际上你在created非要按同步的思维写也不是不行，不过那要用async await，我估计你不会，所以就最简单的了，
+        // 在then(()=>{})里给this.didList赋值完了之后，再调用this.getAllReply()
+        this.getAllReply()
       })
     },
     getRowDynamic() {
@@ -131,39 +138,30 @@ export default {
     getUserInfo() {
       this.admin = JSON.parse(localStorage.getItem('suser'))
     },
-    showReply(){
+    showReply() {
       this.replyVisible = true
     },
 
-    // getAllReply(did){
-    //   this.dynamicData.did = did
-    //   console.log(did)
-    //   selectUserReply(did).then(res => {
-    //     let code = res.data.code
-    //     if(code == 200){
-    //       this.commentData = res.data.data
-    //     } else {
-    //       this.$message({ showClose: true, message: '获取评论失败，请重试!', type: 'error'});
-    //     }
-    //   }).catch((err) => {
-    //     console.log(err)
-    //   })
-    // },
-
     getAllReply() {
-      // did = 1
-      console.log(dynamicInfo)
-      const did = this.dynamicData.did
-      console.log(did)
-      selectUserReply(1).then(res => {
-        let code =  res.data.code
-        if (code == 200){
-          this.commentData = res.data.data.data
-          console.log(this.commentData)
-          console.log(res.data.data)
-        }
-      })
-    }
+      let d = 0
+      for (let j = 0; j < this.didList.length; j++) {
+        // d = this.didList[j]
+        // console.log("d:" + d)
+        // debugger
+        selectUserReply(this.didList[j]).then(res => {
+          let code = res.data.code
+          if (code == 200) {
+            this.commentData = res.data.data.data
+            // console.log(res)
+            // this.commentData.push(res.data.data.data)
+            console.log(this.commentData)
+            // console.log(res.data.data)
+            // console.log(this.commentData[0].replyVOS)
+            // console.log(this.commentData[0].did)
+          }
+        })
+      }
+    },
 
   }
 }
